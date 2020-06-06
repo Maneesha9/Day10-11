52cfecfdecd1c9663225b6da92d9b00402f7de03
Enter Name <maneeshakorla38@gmail.com>
2020-06-06 17:48:07 +0530

    solution

---
+++ uc6.sh
@@ -0,0 +1,17 @@
+#!\bin\bash -x
+declare -A computation
+read -p "enter a: " a
+read -p "enter b: " b
+read -p "enter c: " c
+result=$(($a+$b*$c))
+computation[result]=$result
+echo "a+b*c = $result"
+result1=$(($a*$b+$c))
+computation[result1]=$result1
+echo "a*b+c = $result1"
+result2=$(($c+$a/$b))
+computation[result2]=$result2
+echo "c+a/b = $result2"
+result3=$(($a%$b+$c))
+computation[result3]=$result3
+echo "a%b+c = $result3"
